ed9bf39e0da50aa4c941f7c1ab32b2d735da8e0b
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-12 01:13:27 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,11 @@
 def my_each(array)
   if block_given?
     counter = 0 
-    while counter < array.length do 
+    while counter < array.length
+    yield
+    counter += 1
+    
       
 end
+
+my_each
